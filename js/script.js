6b1ca64b4de62f5b94d919ec675a8ad4c5bbdcbd
Vivek Nair <vnair611@stanford.edu>
2014-09-27 09:59:29 -0700

    Add in the filter assets.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* Filters the given pixels to grayscale.
+ *
+ * Arguments:
+ * pixels -- an array of pixel values
+ */
+function filterGrayscale(pixels) {
+}
+
+/* Brightens the given pixels.
+ *
+ * Arguments:
+ * pixels -- an array of pixel values
+ */
+function filterBrighten(pixels) {
+}
+
+/* Applies a threshold filter to the given pixels. Makes all pixels above
+ * the threshold black and all pixels below the threshold white.
+ *
+ * Arguments:
+ * pixels -- an array of pixel values
+ */
+function filterThreshold(pixels) {
+}
