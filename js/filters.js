6887691e9357936812f16c0ff793018e3c5cf032
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-29 19:14:59 +0530

    Complete filters.

--- js/filters.js
+++ js/filters.js
@@ -1,9 +1,24 @@
+// rgb shift for brightness filter; increase to add more brightness
+var BRIGHTNESS_SHIFT = 40;
+
+// distance threshold from black for threshold filter
+var THRESHOLD_DISTANCE = 100;
+
 /* Filters the given pixels to grayscale.
  *
  * Arguments:
  * pixels -- an array of pixel values
  */
 function filterGrayscale(pixels) {
+  for (var i = 0; i < pixels.length; i += 4) {
+    // use average as grayscale value
+    var grayscale = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+
+    // when all rgb components are the same, the color is grayscale
+    pixels[i] = grayscale;
+    pixels[i + 1] = grayscale;
+    pixels[i + 2] = grayscale;
+  }
 }
 
 /* Brightens the given pixels.
@@ -12,6 +27,12 @@ function filterGrayscale(pixels) {
  * pixels -- an array of pixel values
  */
 function filterBrighten(pixels) {
+  for (var i = 0; i < pixels.length; i += 4) {
+    // add constant adjustment value to all components to increase brightness
+    pixels[i] += BRIGHTNESS_SHIFT;
+    pixels[i + 1] += BRIGHTNESS_SHIFT;
+    pixels[i + 2] += BRIGHTNESS_SHIFT;
+  }
 }
 
 /* Applies a threshold filter to the given pixels. Makes all pixels above
@@ -21,4 +42,24 @@ function filterBrighten(pixels) {
  * pixels -- an array of pixel values
  */
 function filterThreshold(pixels) {
+  for (var i = 0; i < pixels.length; i += 4) {
+    var red = pixels[i];
+    var green = pixels[i + 1];
+    var blue = pixels[i + 2];
+
+    // get cartesian distance from black at (0, 0, 0)
+    var distance = Math.sqrt(red * red + green * green + blue * blue);
+
+    if (distance < THRESHOLD_DISTANCE) {
+      // pixel meets threshold and is close to black; set it to black
+      pixels[i] = 0;
+      pixels[i + 1] = 0;
+      pixels[i + 2] = 0;
+    } else {
+      // pixel exceeds threshold; set it to white
+      pixels[i] = 255;
+      pixels[i + 1] = 255;
+      pixels[i + 2] = 255;
+    }
+  }
 }
