2b559f42028489f8558865d700ecee8116189eff
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-29 19:13:35 +0530

    Switch to two space indentation.

--- js/listeners.js
+++ js/listeners.js
@@ -1,61 +1,61 @@
 (function(window, document, undefined) {
 
-    // base image to filter
-    var image = document.getElementsByTagName('img')[0];
+  // base image to filter
+  var image = document.getElementsByTagName('img')[0];
 
-    // canvas context for drawing
-    var imageCanvas = document.getElementById('image-canvas');
-    var canvasContext = imageCanvas.getContext('2d');
+  // canvas context for drawing
+  var imageCanvas = document.getElementById('image-canvas');
+  var canvasContext = imageCanvas.getContext('2d');
 
-    // DOM filter buttons
-    var grayscale = document.getElementById('grayscale');
-    var brighten = document.getElementById('brighten');
-    var threshold = document.getElementById('threshold');
-    var reset = document.getElementById('reset');
+  // DOM filter buttons
+  var grayscale = document.getElementById('grayscale');
+  var brighten = document.getElementById('brighten');
+  var threshold = document.getElementById('threshold');
+  var reset = document.getElementById('reset');
 
-    /* Returns the image data associated with the canvas. */
-    function getImageData() {
-        return canvasContext.getImageData(0, 0, imageCanvas.width,
-            imageCanvas.height);
-    }
+  /* Returns the image data associated with the canvas. */
+  function getImageData() {
+    return canvasContext.getImageData(0, 0, imageCanvas.width,
+      imageCanvas.height);
+  }
 
-    /* Applies the given filter to the canvas. Updates the canvas' image data
-     * accordingly.
-     *
-     * Arguments:
-     * filter -- function that takes in an array of pixel values and returns
-     *  new, filtered values
-     */
-    function applyFilter(filter) {
-        var data = getImageData();
-        data.data = filter(data.data);
-        canvasContext.putImageData(data, 0, 0);
-    }
+  /* Applies the given filter to the canvas. Updates the canvas' image data
+   * accordingly.
+   *
+   * Arguments:
+   * filter -- function that takes in an array of pixel values and returns
+   *  new, filtered values
+   */
+  function applyFilter(filter) {
+    var data = getImageData();
+    data.data = filter(data.data);
+    canvasContext.putImageData(data, 0, 0);
+  }
 
-    // when window has loaded, the image is ready to be drawn on the canvas
-    window.addEventListener('load', function(event) {
-        canvasContext.drawImage(image, 0, 0);
-         
-        // apply appropriate filters based off which buttons were clicked
-        grayscale.addEventListener('click', function(event) {
-            applyFilter(filterGrayscale);
-            event.preventDefault();
-        });
-
-        brighten.addEventListener('click', function(event) {
-            applyFilter(filterBrighten);
-            event.preventDefault();
-        });
-
-        threshold.addEventListener('click', function(event) {
-            applyFilter(filterThreshold);
-            event.preventDefault();
-        });
-
-        reset.addEventListener('click', function(event) {
-            canvasContext.drawImage(image, 0, 0);
-            event.preventDefault();
-        });
+  // when window has loaded, the image is ready to be drawn on the canvas
+  window.addEventListener('load', function(event) {
+    canvasContext.drawImage(image, 0, 0);
+     
+    // apply appropriate filters based off which buttons were clicked
+    grayscale.addEventListener('click', function(event) {
+      applyFilter(filterGrayscale);
+      event.preventDefault();
     });
+
+    brighten.addEventListener('click', function(event) {
+      applyFilter(filterBrighten);
+      event.preventDefault();
+    });
+
+    threshold.addEventListener('click', function(event) {
+      applyFilter(filterThreshold);
+      event.preventDefault();
+    });
+
+    reset.addEventListener('click', function(event) {
+      canvasContext.drawImage(image, 0, 0);
+      event.preventDefault();
+    });
+  });
 
 })(this, this.document);
--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 # server on 0.0.0.0:8000
 try:
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    print 'Serving on 127.0.0.1:8000'
-    server.serve_forever()
+  server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+  print 'Serving on 127.0.0.1:8000'
+  server.serve_forever()
 except KeyboardInterrupt:
-    server.socket.close()
+  server.socket.close()
