b752a0bdd71f5817cbc36cb116540ad654ae7e77
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-29 21:23:51 +0530

    Complete comments.

--- js/comments.js
+++ js/comments.js
@@ -1,4 +1,78 @@
-// TODO: comments code
+// HTTP OK status code
+var STATUS_OK = 200;
+
+// interval to refresh comments at
+var COMMENTS_REFRESH_INTERVAL = 2000;
+
+var commentForm = document.getElementsByTagName('form')[0];
+var commentList = document.getElementById('comment-list');
+var commentTemplate = document.getElementById('comment-template');
+
+commentForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var nameInput = commentForm.querySelector('input[type="text"]');
+  var messageTextarea = commentForm.querySelector('textarea');
+
+  var name = nameInput.value;
+  var message = messageTextarea.value;
+
+  // ensure both name and comment exist
+  if (!name || !message) {
+    return;
+  }
+
+  // add comment via AJAX
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', function(event) {
+    if (request.status === STATUS_OK) {
+      addCommentToList(name, message);
+      nameInput.value = '';
+      messageTextarea.value = '';
+    }
+  });
+
+  request.open('POST', '/comments');
+  request.setRequestHeader('Content-type', 'application/json');
+  request.send(JSON.stringify({
+    name: name,
+    message: message
+  }));
+});
+
+var lastResponse = null;
+
+(function refreshComments() {
+  // get comments via AJAX
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', function(event) {
+    if (request.status === STATUS_OK) {
+      var comments = JSON.parse(request.responseText);
+      lastResponse = request.responseText;
+
+      commentList.innerHTML = '';
+      comments.forEach(function(comment) {
+        addCommentToList(comment.name, comment.message);
+      });
+    }
+  });
+
+  request.open('GET', '/comments');
+  request.send();
+
+  setTimeout(refreshComments, COMMENTS_REFRESH_INTERVAL);
+})();
+
+/* Adds a comment to the list of comments.
+ *
+ * Arguments:
+ * name -- who posted the comment
+ * message -- the message of the comment
+ */
+function addCommentToList(name, message) {
+  var commentLi = renderComment(name, message);
+  commentList.appendChild(commentLi);
+}
 
 /* Renders a comment with the given name and message to be listed in the
  * #comments div. */
