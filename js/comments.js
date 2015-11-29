b75119549f0d0f030c2e8ca4246c25b4b7da0065
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-29 20:27:07 +0530

    Add comments starter files.

---
+++ js/comments.js
@@ -0,0 +1,67 @@
+// TODO: comments code
+
+/* Renders a comment with the given name and message to be listed in the
+ * #comments div. */
+function renderComment(name, message) {
+  return tag('li', [
+    tag('p', [
+      tag('strong', name),
+      ' ',
+      message
+    ])
+  ]);
+}
+
+/* Creates and returns an HTMLElement representing a tag of the given name.
+ * attrs is an object, where the key-value pairs represent HTML attributes to
+ * set on the tag. contents is an array of strings/HTMLElements (or just a single
+ * string/HTMLElement) that will be contained within the tag.
+ *
+ * Note that attrs is an optional parameter, and can be ommitted.
+ *
+ * Examples:
+ * tag('p', 'A simple paragraph') => <p>A simple paragraph</p>
+ * tag('a', {href: '/about'}, 'About') => <a href="/about">About</a>
+ *
+ * tag('ul', tag('li', 'First item')) => <ul><li>First item</li></ul>
+ *
+ * tag('div', [
+ *   tag('h1', {'class': 'headline'}, 'JavaScript'),
+ *   ' is awesome, ',
+ *   tag('span', "especially in CS42.")
+ * ])
+ * => <div>
+ *      <h1 class="headline">JavaScript</h1>
+ *      is awesome
+ *      <span>especially in CS42.</span>
+ *    </div>
+ */
+function tag(name, attrs, contents) {
+  // attrs is optional
+  if (!contents) {
+    contents = attrs;
+    attrs = [];
+  }
+
+  var element = document.createElement(name);
+  for (var attr in attrs) {
+    element.setAttribute(attr, attrs[attr]);
+  }
+
+  // If contents is a single string or HTMLElement, make it an array of one
+  // element; this guarantees that contents is an array below.
+  if (!(contents instanceof Array)) {
+    contents = [contents];
+  }
+
+  contents.forEach(function(piece) {
+    if (piece instanceof HTMLElement) {
+      element.appendChild(piece);
+    } else {
+      // must create a text node for a raw string
+      element.appendChild(document.createTextNode(piece));
+    }
+  });
+
+  return element;
+}
--- server.py
+++ server.py
@@ -1,9 +1,81 @@
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
+from urlparse import urlparse, parse_qs
+import urllib, urllib2
+import shutil
+import json
+
+# in-memory comments database for simplicity
+comments = []
+
+class ExtendedHTTPHandler(SimpleHTTPRequestHandler):
+  """
+  Serves files in the local directory. Acts a proxy for Flickr images.
+  """
+
+  def __init__(self, *args, **kwargs):
+    """
+    Creates an ExtendedHTTPHandler that serves comments.
+    """
+    SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+
+  def do_GET(self):
+    """
+    Handles GET requests.
+    """
+    if self.path == '/comments':
+      self.send_response(200)
+      self.send_header('Content-type', 'application/json')
+      self.end_headers()
+      self.wfile.write(json.dumps(comments))
+    else:
+      # let SimpleHTTPRequestHandler serve static files
+      return SimpleHTTPRequestHandler.do_GET(self)
+
+  def do_POST(self):
+    """
+    Handles POST requests.
+    """
+    if self.path == '/comments':
+      length = int(self.headers.getheader('content-length', 0))
+      try:
+        data = json.loads(self.rfile.read(length))
+      except ValueError:
+        self.send_response(422)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        error = {'error': 'Must provide a valid json body.'}
+        self.wfile.write(json.dumps(error))
+        return
+
+      name = data.get('name')
+      message = data.get('message')
+
+      if (name is None or message is None or
+          not isinstance(name, basestring) or not isinstance(message, basestring) or
+          name == '' or message == ''):
+        self.send_response(422)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        error = {'error': 'Must provide a valid name and message.'}
+        self.wfile.write(json.dumps(error))
+      else:
+        comments.append({'name': name, 'message': message})
+        self.send_response(200)
+        self.end_headers()
+
+        success = {'success': True}
+        self.wfile.write(json.dumps(success))
+    else:
+      return SimpleHTTPRequestHandler.do_POST(self)
+
 
 # server on 0.0.0.0:8000
 try:
-  server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+  server = HTTPServer(('0.0.0.0', 8000), ExtendedHTTPHandler)
   print 'Serving on 127.0.0.1:8000'
   server.serve_forever()
 except KeyboardInterrupt:
