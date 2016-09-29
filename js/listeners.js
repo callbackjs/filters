54771d67352c9c7ad43506c00c4daf93cb2b6603
Karthik Viswanathan <karthik.ksv@gmail.com>
2016-09-28 18:54:27 -0700

    Convert listeners.js to es6.

--- js/listeners.js
+++ js/listeners.js
@@ -1,20 +1,20 @@
 // base image to filter
-var image = document.getElementsByTagName('img')[0];
+const image = document.getElementsByTagName('img')[0]
 
 // canvas context for drawing
-var imageCanvas = document.getElementById('image-canvas');
-var canvasContext = imageCanvas.getContext('2d');
+const imageCanvas = document.getElementById('image-canvas')
+const canvasContext = imageCanvas.getContext('2d')
 
 // DOM filter buttons
-var grayscale = document.getElementById('grayscale');
-var brighten = document.getElementById('brighten');
-var threshold = document.getElementById('threshold');
-var reset = document.getElementById('reset');
+const grayscale = document.getElementById('grayscale')
+const brighten = document.getElementById('brighten')
+const threshold = document.getElementById('threshold')
+const reset = document.getElementById('reset')
 
 /* Returns the image data associated with the canvas. */
 function getImageData() {
-  return canvasContext.getImageData(0, 0, imageCanvas.width,
-    imageCanvas.height);
+  return canvasContext.getImageData(
+    0, 0, imageCanvas.width, imageCanvas.height)
 }
 
 /* Applies the given filter to the canvas. Updates the canvas' image data
@@ -25,33 +25,33 @@ function getImageData() {
  *  new, filtered values
  */
 function applyFilter(filter) {
-  var data = getImageData();
-  data.data = filter(data.data);
-  canvasContext.putImageData(data, 0, 0);
+  const imageData = getImageData()
+  imageData.data = filter(imageData.data)
+  canvasContext.putImageData(imageData, 0, 0)
 }
 
 // when window has loaded, the image is ready to be drawn on the canvas
-window.addEventListener('load', function(event) {
-  canvasContext.drawImage(image, 0, 0);
+window.addEventListener('load', event => {
+  canvasContext.drawImage(image, 0, 0)
    
   // apply appropriate filters based off which buttons were clicked
-  grayscale.addEventListener('click', function(event) {
-    applyFilter(filterGrayscale);
-    event.preventDefault();
-  });
+  grayscale.addEventListener('click', event => {
+    event.preventDefault()
+    applyFilter(filterGrayscale)
+  })
 
-  brighten.addEventListener('click', function(event) {
-    applyFilter(filterBrighten);
-    event.preventDefault();
-  });
+  brighten.addEventListener('click', event => {
+    event.preventDefault()
+    applyFilter(filterBrighten)
+  })
 
-  threshold.addEventListener('click', function(event) {
-    applyFilter(filterThreshold);
-    event.preventDefault();
-  });
+  threshold.addEventListener('click', event => {
+    event.preventDefault()
+    applyFilter(filterThreshold)
+  })
 
-  reset.addEventListener('click', function(event) {
-    canvasContext.drawImage(image, 0, 0);
-    event.preventDefault();
-  });
-});
+  reset.addEventListener('click', event => {
+    event.preventDefault()
+    canvasContext.drawImage(image, 0, 0)
+  })
+})
